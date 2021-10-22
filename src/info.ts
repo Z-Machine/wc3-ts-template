d27df41616709f528509fbc11728f35c9050cd79
Z-Machine <Z-Machine@users.noreply.github.com>
2021-10-21 22:31:35 -0500

    compiletime example

--- src/info.ts
+++ src/info.ts
@@ -1,10 +1,18 @@
 import { Timer, Unit, Quest } from "w3ts/index";
 import { Players } from "w3ts/globals";
 import { addScriptHook, W3TS_HOOK } from "w3ts/hooks";
+import { ObjectData } from "mdx-m3-viewer/dist/cjs/utils/w3x/objectdata/objectdata";
+import { FetchType, OEUnit } from "mdx-m3-viewer/dist/cjs/utils/w3x/objectdata/objects/unit";
+
+type con = {log: (str: string) => void}
 
 const BUILD_DATE = compiletime(() => new Date().toUTCString());
 const TS_VERSION = compiletime(() => require('typescript').version);
 const TSTL_VERSION = compiletime(() => require('typescript-to-lua').version);
+const PEASANT = compiletime(({objectData, log}: {objectData: ObjectData, log: (s: string) => void }) => {
+  log("Hello Compiletime!");
+  return objectData.units.get("hpea")?.fetch();
+}) as FetchType;
 
 addScriptHook(W3TS_HOOK.MAIN_AFTER, () => {
   const info = new Quest();
@@ -16,6 +24,15 @@ addScriptHook(W3TS_HOOK.MAIN_AFTER, () => {
   info.addItem(`Typescript: v${TS_VERSION}`);
   info.addItem(`Transpiler: v${TSTL_VERSION}`);
   info.setDescription(`Map by Zed#9133`);
+
+  const moreInfo = new Quest();
+  moreInfo.required = true;
+  moreInfo.enabled = true;
+  moreInfo.setIcon(PEASANT.iconGameInterface);
+  moreInfo.setTitle(PEASANT.name);
+  moreInfo.addItem(PEASANT.modelFile);
+  moreInfo.addItem(PEASANT.tooltipBasic);
+  moreInfo.setDescription(PEASANT.tooltipExtended);
 
   print("Welcome to TypeScript!");
 
